400467ec99ade2f39b71caaf9e592d25fec0e856
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-24 22:06:22 -0300

    Atualizar

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,44 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact ';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+
+      </ul>
+
+      
+      <Routes>
+        <Route exact path="/"  />
+        <Home />
+      </Routes>
+
+      <Routes>
+        <Route exact path="/company"  />
+        <Company />
+      </Routes>
+
+      <Routes>
+        <Route exact path="/contact"  />
+        <Contact/>
+      </Routes>
+
+      <Routes>
+      <Route exact path="/newproject"  />
+        <NewProject/>
+      </Routes>
+
+     
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
