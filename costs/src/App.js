af83742be56e5f4caf7ff59951d307f1c64af83c
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-24 22:17:34 -0300

    Atualizar

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/pages/Home';
-import Contact from './components/pages/Contact ';
+import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 
@@ -16,26 +16,12 @@ function App() {
 
       
       <Routes>
-        <Route exact path="/"  />
-        <Home />
+        <Route exact path="/"  element={<Home/>}/>
+        <Route  path="/company"  element={<Company/>}/>
+        <Route  path="/contact" element={<Contact/>} />
+        <Route  path="/newproject" element={<NewProject/>} />
+        
       </Routes>
-
-      <Routes>
-        <Route exact path="/company"  />
-        <Company />
-      </Routes>
-
-      <Routes>
-        <Route exact path="/contact"  />
-        <Contact/>
-      </Routes>
-
-      <Routes>
-      <Route exact path="/newproject"  />
-        <NewProject/>
-      </Routes>
-
-     
 
       <p>Footer</p>
     </Router>
