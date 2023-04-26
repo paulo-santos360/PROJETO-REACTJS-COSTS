2e807d9c954f56d367f3d06265d7bbfa30b74146
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-26 19:57:42 -0300

    atualizar

--- costs/src/components/layout/Navbar.js
+++ costs/src/components/layout/Navbar.js
@@ -1,35 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import Container from "./Container"
+import Container from "./Container";
 
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
-import logo from '../../img/costs_logo.png'
+import logo from "../../img/costs_logo.png";
 
-function Navbar(){
-    return(
-        <nav>
-            <Container>
-            <Link to='/'>
-                <img src={logo} alt="Costs" />
-            </Link>
-            <ul>
-                <li>
-            <Link to='/'>Home</Link>
-            </li>
-            <li>
-            <Link to='/contact'>Contato</Link>
-            </li>
-            <li>
-            <Link to='/company'>Empresa</Link>
-            </li>
-            <li>
-            <Link to='/projeto'>Novo Projeto</Link>
-            </li>
-            </ul>
-            </Container>
-        </nav>
-    )
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="Costs" />
+        </Link>
+        <ul className={styles.list}>
+          <li  className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contato</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Empresa</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/projeto">Novo Projeto</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
