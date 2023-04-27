976f3c35cdc32a903176445c25ad5e1ef9545315
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-27 16:33:55 -0300

    Atualizar

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,14 @@
+import Input from '../form/Input'
+
+import styles from './ProjectForm.module.css'
+
 function ProjectForm(){
+
     return(
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto"/>
-            </div>
+        <form className={styles.form}>
+
+            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
+           
             <div>
                 <input type="number" placeholder="Insira o orçamento total"/>
             </div>
